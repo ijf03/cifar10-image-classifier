a3a51b9fc819dc1d48070e264a4db0d84b685b66
ijf03 <63137224+ijf03@users.noreply.github.com>
2026-09-06 17:24:33 +1000

    Working CPU PyTorch Env with Nix

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+	description = "CPU-only Python environment for a CIFAR-10 classifier";
+
+	inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+
+	outputs = { nixpkgs, ... }:
+		let
+			system = "x86_64-linux";
+			pkgs = import nixpkgs {
+				inherit system;
+				config.cudaSupport = false;
+			};
+			python = pkgs.python3.withPackages (ps: with ps; [
+				torch
+				torchvision
+			]);
+		in
+		{
+			devShells.${system}.default = pkgs.mkShell {
+				packages = [ python ];
+			};
+		};
+}
